47b065cfbb07bdfe71c9fb185afb66e6cf6ff73a
Francis Cruz <fcruz@blend.com>
2023-03-11 20:47:52 -0800

    remove logger

--- apps/storefront/src/pages/index.tsx
+++ apps/storefront/src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { log } from "logger";
 import Head from "next/head";
 import { CounterButton, NewTabLink } from "ui";
 
 export default function Store() {
-  log("Hey! This is Home.");
+  console.log("Hey! This is Home.");
   return (
     <div className="container">
       <Head>
